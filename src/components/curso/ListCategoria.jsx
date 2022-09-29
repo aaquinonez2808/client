d4d6d4cb2a7e55cd5f53305d41c5271ece072e4c
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2022-09-29 10:53:51 -0500

    ultima-verwsion

--- src/components/curso/ListCategoria.jsx
+++ src/components/curso/ListCategoria.jsx
@@ -9,7 +9,7 @@ const ListCategoria = () => {
         <ul className='navbar-nav'>
             {categorias.map((categoria) => (
                 <li className='nav-item' key={categoria.id}>
-                    <div className='mb-3 p-5'>
+                    <div className='mb-3 p-5'S>
                         <h3>{categoria.nombre}</h3>
                         <p>{categoria.descripcion}</p>
                         <NavLink className="btn btn-primary d-block w-25" to={`/categorias/${categoria.id}/cursos`}>
